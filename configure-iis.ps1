687a6c2f104a632720648639bfa26f43b1756feb
grbb1127 <grbb1127@gmail.com>
2020-10-24 18:12:59 +0800

    add

---
+++ configure-iis.ps1
@@ -0,0 +1,8 @@
+
+# Install IIS.
+dism /online /enable-feature /featurename:IIS-WebServerRole
+
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Welcome to Azure! My name is s1710931002 20201024 $($env:computername).</h2></body></html>"
