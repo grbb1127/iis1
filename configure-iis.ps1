c4000edad82f809132fe78b6d64ab9b0ec9c8b39
grbb1127 <grbb1127@gmail.com>
2020-11-13 23:38:41 +0800

    fix

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -5,4 +5,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body></body></html>"
+  -Value '<html><body><iframe width="1024" height="804" src="https://app.powerbi.com/view?r=eyJrIjoiN2M0NTdhOGYtYmVmOC00NTMyLWIzODctYzk3MTY2YTc2MzRhIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
