051da88e77aa50b9eb8af73ee2685e8273cc52c9
grbb1127 <grbb1127@gmail.com>
2020-11-14 13:46:48 +0800

    add

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -5,4 +5,4 @@ dism /online /enable-feature /featurename:IIS-WebServerRole
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value '<html><body><iframe width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMmVlMDgxYTItODM5My00ZDcwLTliOTgtYmRkODAyYzhhZTM3IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>'
+  -Value '<html><body><h2>Welcome to Azure! My name is s1710931002 yaojujun.</h2>(報表)</body></html>'
